74373b137cd457ae167b84a58646a47069cabf89
Максим <izver_jobva@icloud.com>
2024-02-19 22:40:28 +0300

    Выполнено первое задание практики

---
+++ base-syntacx/src/ru/sber/base/syntax/Task5.java
@@ -0,0 +1,28 @@
+package ru.sber.base.syntax;
+import java.util.Scanner;
+
+public class Task5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите количество чисел: ");
+        int n = scanner.nextInt();
+
+        if(n <= 2 || n >= 100){
+            System.out.print("Введено недопустимое число " + n + ". Допустимый диапазон значений: 2 - 100");
+        } else {
+            int first = 1;
+            int second = 1;
+
+            System.out.print("Первые " + n + " чисел Фибоначчи: ");
+
+            for (int i = 0; i < n; i++) {
+                System.out.print(first + " ");
+                int next = first + second;
+                first = second;
+                second = next;
+            }
+        }
+
+    }
+}
