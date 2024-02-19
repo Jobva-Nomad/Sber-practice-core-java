119e0035df17e931c5990e1ce4273a661355ac1e
Максим <izver_jobva@icloud.com>
2024-02-19 21:34:08 +0300

    Выполнено первое задание практики

--- base-syntacx/src/ru/sber/base/syntax/Task3.java
+++ base-syntacx/src/ru/sber/base/syntax/Task3.java
@@ -1,5 +1,14 @@
 package ru.sber.base.syntax;
+import java.util.Scanner;
 
 public class Task3 {
+    public static void main(String[] args){
+        System.out.println("* * * * * * * * * *");
+        System.out.println(" * * * * * * * * * ");
+        System.out.println("* * * * * * * * * *");
+        System.out.println(" * * * * * * * * * ");
+        System.out.println("* * * * * * * * * *");
+        System.out.println(" * * * * * * * * * ");
 
+    }
 }
---
+++ base-syntacx/src/ru/sber/base/syntax/Task4.java
@@ -0,0 +1,28 @@
+package ru.sber.base.syntax;
+import java.util.Scanner;
+
+public class Task4 {
+    public static void main(String[] args) {
+        int temp;
+        boolean isPrime = true;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите число, а я определю, простое ли оно: ");
+        int num = scanner.nextInt();
+
+       for(int i = 2; i <= num/2; i++) {
+           temp = num % i;
+           if(temp == 0){
+               isPrime = false;
+               break;
+           }
+
+       }
+        if(isPrime){
+            System.out.println("да, простое");
+        } else {
+            System.out.println("нет, составное");
+        }
+
+    }
+}
